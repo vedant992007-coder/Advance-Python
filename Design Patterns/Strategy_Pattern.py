1d45d7846b8d039460c9f5fb0aa1be9df4611c51
vedant992007-coder <vedant992007@gmail.com>
2026-07-29 01:29:55 +0530

    Update Strategy_Pattern.py
    Paid ₹1000 using Credit Card
    Paid ₹500 using UPI
    Paid ₹200 using Cash

--- Design Patterns/Strategy_Pattern.py
+++ Design Patterns/Strategy_Pattern.py
@@ -1,1 +1,49 @@
+from abc import ABC, abstractmethod
+
+# Strategy Interface
+class PaymentStrategy(ABC):
+
+    @abstractmethod
+    def pay(self, amount):
+        pass
+
+
+# Concrete Strategies
+class CreditCard(PaymentStrategy):
+
+    def pay(self, amount):
+        print(f"Paid ₹{amount} using Credit Card")
+
+
+class UPI(PaymentStrategy):
+
+    def pay(self, amount):
+        print(f"Paid ₹{amount} using UPI")
+
+
+class Cash(PaymentStrategy):
+
+    def pay(self, amount):
+        print(f"Paid ₹{amount} using Cash")
+
+
+# Context
+class ShoppingCart:
+
+    def __init__(self, strategy):
+        self.strategy = strategy
+
+    def checkout(self, amount):
+        self.strategy.pay(amount)
+
+
+# Client Code
+cart = ShoppingCart(CreditCard())
+cart.checkout(1000)
+
+cart = ShoppingCart(UPI())
+cart.checkout(500)
+
+cart = ShoppingCart(Cash())
+cart.checkout(200)
 
