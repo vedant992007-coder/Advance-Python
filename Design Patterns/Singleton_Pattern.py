c1a7764eb94ff41ab72912c767891b5c7d76e10f
vedant992007-coder <vedant992007@gmail.com>
2026-07-29 01:26:42 +0530

    Update Singleton_Pattern.py
    Initializing Control Tower!
    True

--- Design Patterns/Singleton_Pattern.py
+++ Design Patterns/Singleton_Pattern.py
@@ -1,1 +1,18 @@
+#Singleton Pattern - 
+
+class ControlTower():
+    _instance = None
+
+    def __new__(cls):
+        if cls._instance is None: # Check if an instance already exists
+            cls._instance = super().__new__(cls)
+            print("Initializing Control Tower!")
+        return cls._instance
+
+tower1 = ControlTower()
+tower2 = ControlTower()
+
+
+print(tower1 is tower2)
+
 
