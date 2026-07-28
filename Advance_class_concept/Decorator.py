6525d92ec0bb50ca5835aa26fb8af8101111f088
vedant992007-coder <vedant992007@gmail.com>
2026-07-29 00:43:25 +0530

    Implement decorator for greet function
    Added a decorator function to wrap the greet function, demonstrating pre- and post-execution behavior.

--- Advance_class_concept/Decorator.py
+++ Advance_class_concept/Decorator.py
@@ -1,1 +1,21 @@
+# Decorator function
+def my_decorator(func):
+    def wrapper():
+        print("Before function")
+        func()
+        print("After function")
+    return wrapper
 
+# Applying the decorator
+@my_decorator
+def greet():
+    print("Hello!")
+
+# Calling the function
+greet()
+
+
+#Output - 
+#Before function
+#Hello!
+#After function
