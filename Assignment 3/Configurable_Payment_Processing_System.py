989830b05a93bb3556bd506f4d946561c7616534
vedant992007-coder <vedant992007@gmail.com>
2026-07-29 01:09:02 +0530

    Update Configurable_Payment_Processing_System.py
    ### Payment Processing System ###
    1. Credit Card
    2. Debit Card
    3. UPI
    4. Net Banking
    5. Exit
    Enter your choice: 1
    Enter payment amount: 5000
    Payment of $5000.0 processed using Credit Card.
    
    ### Payment Processing System ###
    1. Credit Card
    2. Debit Card
    3. UPI
    4. Net Banking
    5. Exit
    Enter your choice: 2
    Enter payment amount: 5000
    Payment of $5000.0 processed using Debit Card.
    
    ### Payment Processing System ###
    1. Credit Card
    2. Debit Card
    3. UPI
    4. Net Banking
    5. Exit
    Enter your choice: 3
    Enter payment amount: 5000
    Payment of $5000.0 processed using UPI.
    
    ### Payment Processing System ###
    1. Credit Card
    2. Debit Card
    3. UPI
    4. Net Banking
    5. Exit
    Enter your choice: 4
    Enter payment amount: 5000
    Payment of $5000.0 processed using Net Banking.
    
    ### Payment Processing System ###
    1. Credit Card
    2. Debit Card
    3. UPI
    4. Net Banking
    5. Exit
    Enter your choice: 5
    Thank you for using the Payment System!

--- Assignment 3/Configurable_Payment_Processing_System.py
+++ Assignment 3/Configurable_Payment_Processing_System.py
@@ -1,1 +1,77 @@
+from abc import ABC, abstractmethod
+
+class PaymentStrategy(ABC):
+    @abstractmethod
+    def pay(self, amount):
+        pass
+
+
+class CreditCardPayment(PaymentStrategy):
+    def pay(self, amount):
+        print(f"Payment of ${amount} processed using Credit Card.")
+
+class DebitCardPayment(PaymentStrategy):
+    def pay(self, amount):
+        print(f"Payment of ${amount} processed using Debit Card.")
+
+class UPI(PaymentStrategy):
+    def pay(self, amount):
+        print(f"Payment of ${amount} processed using UPI.")
+
+class NetBanking(PaymentStrategy):
+    def pay(self, amount):
+        print(f"Payment of ${amount} processed using Net Banking.")
+
+
+class PaymentProcessor:
+    def __init__(self, strategy= None):
+        self.strategy = strategy
+
+    def set_strategy(self, strategy):
+        self.strategy =  strategy
+
+    def pay(self, amount): 
+        if self.strategy is None:
+            print("please select payment method")
+
+        else:
+            self.strategy.pay(amount)
+
+
+
+payment_method = PaymentProcessor()
+
+
+while True:
+    Choice = input("""Enter Payment Method 
+                1. Credit Card
+                2. Debit Card
+                3. UPI
+                4. Net Banking
+                5. Exit \n """
+                   )
+    if Choice == "5":
+        print("exit")
+        break
+
+    if Choice == "1":
+        payment_method.set_strategy(CreditCardPayment())
+    elif Choice == "2":
+        payment_method.set_strategy(DebitCardPayment())
+    elif Choice == "3":
+        payment_method.set_strategy(UPI())
+    elif Choice == "4":
+        payment_method.set_strategy(NetBanking())
+    else:
+        print("invalid choice")
+        continue
+
+    try:
+        amount = float(input("Enter the amount to be paid: "))
+    except ValueError:
+        print("Invalid amount")
+        continue
+
+    payment_method.pay(amount)
+
 
