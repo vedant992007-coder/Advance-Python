c2afb234c54f591c3bf61f089acb1ad8e6ef0c0d
vedant992007-coder <vedant992007@gmail.com>
2026-07-29 01:39:02 +0530

    Implement Car class with price management methods
    === Original Car Details ===
    Car Brand: Toyota
    Car Price: ₹ 1500000
    
    Car Brand: Hyundai
    Car Price: ₹ 1200000
    
    Car Brand: Honda
    Car Price: ₹ 1400000
    
    Car Brand: Tata
    Car Price: ₹ 1000000
    
    Price of Toyota increased by ₹100000
    
    Price of Tata decreased by ₹50000
    
    === Updated Car Details ===
    Car Brand: Toyota
    Car Price: ₹ 1600000
    
    Car Brand: Hyundai
    Car Price: ₹ 1200000
    
    Car Brand: Honda
    Car Price: ₹ 1400000
    
    Car Brand: Tata
    Car Price: ₹ 950000

--- OOP_concept/Car_info.py
+++ OOP_concept/Car_info.py
@@ -1,1 +1,60 @@
+class Car:
+    
+    def __init__(self, brand, price):
+        self.brand = brand
+        self.price = price
+
+    
+    def display_info(self):
+        print("Car Brand:", self.brand)
+        print("Car Price: ₹", self.price)
+
+    
+    def increment_price(self, amount):
+        self.price += amount
+        print(f"\nPrice of {self.brand} increased by ₹{amount}")
+
+    
+    def decrement_price(self, amount):
+        self.price -= amount
+        print(f"\nPrice of {self.brand} decreased by ₹{amount}")
+
+
+
+c1 = Car("Toyota", 1500000)
+c2 = Car("Hyundai", 1200000)
+c3 = Car("Honda", 1400000)
+c4 = Car("Tata", 1000000)
+
+
+print("=== Original Car Details ===")
+c1.display_info()
+print()
+
+c2.display_info()
+print()
+
+c3.display_info()
+print()
+
+c4.display_info()
+
+
+c1.increment_price(100000)
+
+
+c4.decrement_price(50000)
+
+
+print("\n=== Updated Car Details ===")
+c1.display_info()
+print()
+
+c2.display_info()
+print()
+
+c3.display_info()
+print()
+
+c4.display_info()
 
